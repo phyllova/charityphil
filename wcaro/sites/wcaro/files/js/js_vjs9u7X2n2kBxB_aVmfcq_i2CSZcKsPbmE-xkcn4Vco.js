b6a93930c00efeb9e5c4cd7570b94b1d50ac997f
Phy Llova <jenshllova@acelinkcourier.com>
2024-03-03 01:38:59 -0800

    added files

--- wcaro/sites/wcaro/files/js/js_vjs9u7X2n2kBxB_aVmfcq_i2CSZcKsPbmE-xkcn4Vco.js
+++
@@ -1,3 +0,0 @@
-/* Source and licensing information for the line(s) below can be found at https://help.unicef.org/wcaro/libraries/custom/consoleLogger.js. */
-(function($,window,Drupal,drupalSettings){$.fn.consoleLogger=function(data){return this.each(function(){if(typeof drupalSettings.unicef_donation_form!=='undefined')if(typeof drupalSettings.unicef_donation_form.debug_form!=='undefined'){var debug_form=drupalSettings.unicef_donation_form.debug_form;if(debug_form)console.log(data)}})}})(jQuery,window,Drupal,drupalSettings)
-/* Source and licensing information for the above line(s) can be found at https://help.unicef.org/wcaro/libraries/custom/consoleLogger.js. */;
